66adcf1264543b1f85c825c9a8db68ae64d25f26
Riccardo P <riccardop@outlook.com>
2024-07-08 11:31:07 +1000

    website-template: added various CSS styling

--- website-template/src/routes/HomePage.tsx
+++ website-template/src/routes/HomePage.tsx
@@ -2,12 +2,90 @@ const HomePage = () => {
   return (
     <main>
       <section>
-        <h1>Website Example</h1>
+        <h1>Website Template</h1>
         <p>
-          This is a typical website featuring a navigation and few pages with
-          different content.
+          The objective of this project is to demonstrate one of many{' '}
+          <strong>typical setup for a React website</strong> using{' '}
+          <code>react-router-dom</code> to manage routes, and a combination of
+          React's <code>reducer</code> and <code>context</code> API to manage
+          the state.
         </p>
-        <p>Each page loads its own content from the corresponding endpoint.</p>
+        <p>The website uses the JSONPlaceholder API to handle data requests.</p>
+        <p>
+          Styling is implemented using CSS modules for each component, as well
+          as a global CSS reset, common styles, and CSS variables.
+        </p>
+      </section>
+      <section>
+        <h2>Folder structure:</h2>
+        <ul>
+          <li>
+            <h3>components</h3>
+            <p>
+              Contains and organizes all React components created for the
+              website.
+            </p>
+          </li>
+          <li>
+            <h3>context</h3>
+            <p>
+              Contains the <code>DataContext</code> file, which, in combination
+              with the reducer, provides <code>state</code> and{' '}
+              <code>dispatch</code> to the application to manage and consume
+              state data.
+            </p>
+          </li>
+          <li>
+            <h3>reducer</h3>
+            <p>
+              Contains the <code>reducer</code> and <code>actions</code> used to
+              modify and update the state.
+            </p>
+          </li>
+          <li>
+            <h3>routes</h3>
+            <p>Contains all the routes (pages) featured in the website.</p>
+          </li>
+          <li>
+            <h3>state</h3>
+            <p>Contains the initial application's state.</p>
+          </li>
+          <li>
+            <h3>types</h3>
+            <p>
+              Contains all the TypeScript types utilized in different parts of
+              the application.
+            </p>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h2>Usage</h2>
+        <p>
+          This project is built with <strong>Vite</strong>.
+        </p>
+        <p>To use this project, follow these steps:</p>
+        <ol>
+          <li>
+            <p>Clone the repository to your local machine.</p>
+          </li>
+          <li>
+            <p>
+              Install dependencies using <code>npm install</code>.
+            </p>
+          </li>
+          <li>
+            <p>
+              Run the project using <code>npm run dev</code>.
+            </p>
+          </li>
+          <li>
+            <p>
+              Open your web browser and navigate to the localhost url displayed
+              in your terminal.
+            </p>
+          </li>
+        </ol>
       </section>
     </main>
   );
